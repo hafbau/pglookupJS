dfc1ddbecbfa90c71c628fa5ae7161098df6e2de
Hafiz Suara <hafbau@yahoo.com>
2017-02-01 05:13:59 +0000

    create mileston table db migration

--- knexfile.js
+++ knexfile.js
@@ -3,9 +3,14 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
-      filename: './dev.sqlite3'
+      database: 'vagrant',
+      user:     'development',
+      password: 'development',
+      host: 'localhost',
+      port: 5432,
+      ssl: true
     }
   },
 
